7cfe56dee0dede3d51ce766f7420aea86577852d
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2017-08-23 18:56:28 +0530

    Primes upto n

---
+++ Control Flow/if-else.py
@@ -0,0 +1,11 @@
+a = int(input('Primes from 2 to n : '))
+for i in range(1, a+1):
+    count = 0
+    for j in range(1, i):
+        if i % j == 0:
+            count += 1
+
+    if count == 1:
+        print(i)
+
+
