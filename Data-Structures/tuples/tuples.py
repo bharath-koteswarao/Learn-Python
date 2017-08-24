662f837f933b089539da8a99f2c3beb1f7dda77e
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2017-08-24 18:51:31 +0530

    Tuples

---
+++ Data-Structures/tuples/tuples.py
@@ -0,0 +1,20 @@
+"""Tuple with one element should contain a comma after that element"""
+
+names = 'bk', 'bharath', 'bvsbrk'
+
+"""It can also be initialized like this :
+        "bk","bharath","bvsbrk" = names
+"""
+
+# To access elements of tuple just use names(0)
+
+# Tuples are immutable.We can't even add elements to tuple during runtime
+
+# len() is used to find the length of tuples
+
+tup = "only element in tup"
+
+print(names)
+print(names[0])
+
+print(tup)
