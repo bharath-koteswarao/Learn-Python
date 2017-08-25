6380f3a78f49d8b6c31914ac4d0b7fffa10346d4
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2017-08-26 00:23:39 +0530

    Operators

---
+++ Basics/operators.py
@@ -0,0 +1,24 @@
+# or
+
+print(2 & 3)
+
+# or
+
+print(10 | 20)
+
+"""
+Right shift operator shifts the k rightmost bits
+n >> k means n/(2^k)
+"""
+
+print(10 >> 1)
+
+"""
+Left shift operator shifts the k rightmost bits
+n >> k means n*(2^k)
+"""
+print(10 << 1)
+
+# !, ^(XOR) are some other operators
+
+# Python doesn't have ++ -- operators
