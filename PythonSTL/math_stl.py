0602b3b5214453d7b74311d65e14651d6f305741
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2017-08-25 18:49:17 +0530

    math module

---
+++ PythonSTL/math_stl.py
@@ -0,0 +1,15 @@
+import math
+import random
+import statistics
+
+print(math.pi / 4)
+
+print(math.cos(math.pi))
+
+print(random.random())
+
+items = [1, 2, 3, 4, 5]
+
+print(statistics.mean(items))
+
+print(statistics.variance(items, statistics.mean(items)))
